32f5205f0539b49f1ff867fd1da7e74fb8748e7d
Bweezy1 <118490510+Bweezy1@users.noreply.github.com>
2023-11-22 15:16:21 -0600

    Update csmaca.tcl
    WIP (code needs testing)

--- csmaca.tcl
+++ csmaca.tcl
@@ -12,21 +12,21 @@ set val(mac)          Mac/802_11               ;# MAC type
 set val(rp)           DSDV                     ;# ad-hoc routing protocol 
 set val(nn)           2                        ;# number of mobilenodes
 
-#create new simulation
+#Create new simulation
 set ns [new simulator]
 
-#open a NAM trace file
+#Open a NAM trace file
 set tracefile [open .out.nam w]
 $ns nametrace-all $tracefile
 
-#set up a topology
+#Set up a topology
 set topo [new Topography]
 $topo load_flatgrid 500 500
 
-#setup up god (general operations director)
+#Setup up god (general operations director)
 create-god $val(nn)
 
-#node configuration
+#Node configuration (wireless node setup)
 $ns_ node-config
                   -adhocRouting $val(rp) \
                   -llType	   $val(ll) \
@@ -43,14 +43,14 @@ $ns_ node-config
                   -macTrace       OFF \
                   -movementTrace  OFF
 
-#create two nodes
+#Create two nodes
 set node_($0) [$ns_ node ]
 $node_($0) random-motion 0
 
 set node_($1) [$ns_ node ]
 $node_($1) random-motion 0
 
-#set initial node positions
+#Set initial node positions
 $node_(0) set X_ 10.0
 $node_(0) set Y_ 15.0
 $node_(0) set Z_ 0.0
@@ -59,8 +59,33 @@ $node_(1) set X_ 420.0
 $node_(1) set Y_ 69.0
 $node_(1) set Z_ 0.0
 
-#set node movements
+#Set node movements
 $ns_ at 15.0 "$node_(1) setdest 50.0 25.0 10.0"
 $ns_ at 10.0 "$node_(0) setdest 69.0 42.0 25.0"
 
 $ns at 25.0 "$node_(1) set dest 485.0 490.0 100.0
+
+#Set up TCP connections
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+set sink [new Agent/TCPSink]
+$ns_ attach-agent $node_(0) $tcp
+$ns_ attach-agent $node_(1) $sink
+$ns_ connect $tcp $sink
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns_ at 10.0 "$ftp start"
+
+#Tell the nodes when simulation is over
+$ns_ at 100.0 "$node_($0) reset"
+$ns_ at 100.0 "$node_($1) reset"
+$ns_ at 100.0001 "stop"
+$ns_ at 100.0002 "puts \ "NS EXITING...\" ; $ns_ halt"
+proc stop {} {
+  global ns_ tracefile
+  close $tracefile
+}
+
+#Command to start the simulation
+puts "Starting SImulaion..."
+$ns_ run
